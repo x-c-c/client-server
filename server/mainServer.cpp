9d369b90ee506deda17558a9a3c81ead81d46faa
x-c-c <mipodotovic@gmail.com>
2026-04-08 21:59:08 +0300

    added defaultCongig.json

--- server/mainServer.cpp
+++ server/mainServer.cpp
@@ -1,7 +1,22 @@
 #include <QCoreApplication>
-
+#include "common/ConfigManager.h"
+#include "common/Logger.h"
 int main(int argc, char* argv[])
 {
 	QCoreApplication  application(argc, argv);
+	
+	// Загружаем конфигурацию
+	if(!ConfigManager::instance().loadConfigFromFile("config/defaultConfig.json"))
+	{
+		qCritical() << "Failed to load config file";
+		return 1;
+	}
+	
+	// Инициализируем логгер
+	Logger::init("config/defaultConfig.json");
+	LOG_INFO("Server starting");
+	
+	
+	
 	return application.exec();
 }
